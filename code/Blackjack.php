53f53a05bd42f19c5fba0dbf62e90e6dbe5fe525
MagaSu <magamag268@gmail.com>
2021-10-28 12:27:08 +0200

    Added player.php and blackJack.php

---
+++ code/Blackjack.php
@@ -0,0 +1,12 @@
+<?php
+
+class Blackjack {
+  private $player; // Player
+  private $dealer; // Player for now
+  private $deck; // Deck
+
+  public $getPlayer; //returns the `player` object
+  public $getDealer; //returns the `dealer` object
+  public $getDeck; //returns the `deck` object
+
+}
---
+++ code/Player.php
@@ -0,0 +1,11 @@
+<?php
+
+class Player {
+  private $cards = [];
+  private $lost = false;
+
+  public $hit;
+  public $surrender;
+  public $getScore;
+  public $hasLost;
+}
